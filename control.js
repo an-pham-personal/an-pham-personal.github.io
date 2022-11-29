d688e4e3ea59e3adccdb7a1710b0fbe45f624844
An Pham <anpham.dev@gmail.com>
2022-11-29 23:23:24 +0700

    Full template

--- control.js
+++ control.js
@@ -63,13 +63,11 @@ const query = () => {
   const values = {};
 
   for (let [k, _] of Object.entries(labels)) {
-    console.log(`#${k}`);
     const s = $(`#${k}`);
     if (s.val() != -1) {
       values[k] = s.val();
     }
   }
-  console.log(values);
 
   let sql = [
     "select m.*",
@@ -95,7 +93,6 @@ const query = () => {
 
   for (let [k, v] of Object.entries(values)) {
     if (k === "state") {
-      console.log(k, v);
       conds.push(`h.state = '${v}'`);
     } else {
       conds.push(`${k}_id = ${v}`);
@@ -105,10 +102,11 @@ const query = () => {
 
   sql = sql.join(" ");
   sql = `${sql} ${conds.join(" and ")}`;
-  sql = `${sql} order by price desc limit 5`;
+  sql = `${sql} order by price desc`;
   console.log(sql);
   const res = db.exec(sql);
   console.log(res);
+  listResult(res);
 };
 
 const setupDropdown = (id, data) => {
@@ -128,28 +126,56 @@ const setupDropdown = (id, data) => {
   $(s).on("change", query);
 };
 
+const extractResult = (cols, r) => {
+  const vals = {
+    name: 1,
+    hospital_rating: 1,
+    address: 1,
+    city: 1,
+    state: 1,
+    phone: 1,
+    zip: 1,
+    hospital_id: 1,
+    hospital_rating: 1,
+    hospital_type: 1,
+    hospital_ownership: 1,
+    emergency_services: 1,
+    beds: 1,
+    price: 1,
+    predicted: 1,
+  };
+  Object.keys(vals).forEach((k) => (vals[k] = r[cols.indexOf(k)]));
+  console.log(vals);
+  return vals;
+};
+
 const listResult = (results) => {
-  const tmpl = [];
   const res = $("#result");
   const cols = results[0].columns;
+  let seen = new Set();
+  let counter = 0;
 
-  results[0].values.forEach((r) => {
-    const name = r[cols.indexOf("name")];
-    const rating = r[cols.indexOf("hospital_rating")];
+  for (let r of results[0].values) {
+    const v = extractResult(cols, r);
 
-    const row = [
+    if (seen.has(v.hospital_id)) {
+      continue;
+    }
+    seen.add(v.hospital_id);
+
+    let row = [
       `<div class="result-row row border-bottom p-1 m-1">`,
       `<div class="hospital p-1">`,
-      `<div class="name h5">${name}</div>`,
+      `<div class="name h5">${v.name}</div>`,
       `<div class="rating">`,
       `<span class="val">`,
     ];
 
     for (let i = 0; i < 5; i++) {
-      if (i < rating) {
-        row.push(`<span class="fa fa-star checked"></span>`);
+      if (i < v.hospital_rating) {
+        row.push(`<span class="fa fa-star checked"></span> `);
       } else {
-        row.push(`<span class="fa fa-star"></span>`);
+        row.push(`<span class="fa fa-star"></span> `);
       }
     }
 
@@ -159,52 +185,116 @@ const listResult = (results) => {
       `</div>`,
       `<div class="address">`,
       `<a class="text-decoration-none" target="_blank"`,
-      `href="https://maps.google.com/?q=`,
+      `href="https://maps.google.com/?q=${v.name}, ${v.address}, ${v.city}, ${v.state} ${v.zip}">`,
+      `<p>`,
+      `<i class="fa-solid fa-location-dot text-info"></i> `,
+      `<span class="text-muted">`,
+      `${v.address}, ${v.city}, ${v.state} ${v.zip} - ${v.phone}</span>`,
+      `</p>`,
+      `</a>`,
+      `</div>`,
+      `<div class="desc text-muted">`,
+      `<i class="fa-solid fa-hospital"></i> `,
+      `<span class="type">${v.hospital_type}</span>`,
+      `<span class="ownership"><small>- ${v.hospital_ownership}</small></span>`,
+      `</div>`,
+      `<div class="desc text-muted">`,
+      `<i class="fa-solid fa-truck-medical"></i> `,
+      `<span class="attr">Emergency Service? </span>`,
+      `<span class="val">`,
     ];
-  });
 
-  const VT = [
-    '                href="https://maps.google.com/?q=METHODIST HOSPITAL UNION COUNTY,4604 US HIGHWAY 60 WEST,MORGANFIELD,Kentucky, KY, 42437"',
-    "              >",
-    "                <p>",
-    '                  <i class="fa-solid fa-location-dot text-info"></i>',
-    '                  <span class="text-muted"',
-    "                    >4604 US HIGHWAY 60 WEST, MORGANFIELD, KY 42437 - (123)",
-    "                    456-789",
-    "                  </span>",
-    "                </p>",
-    "              </a>",
-    "            </div>",
-    "",
-    '            <div class="desc text-muted">',
-    '              <i class="fa-solid fa-hospital"></i>',
-    '              <span class="type">Critical Access Hospitals</span>',
-    '              <span class="ownership"',
-    "                ><small>- Voluntary non-profit - Church</small></span",
-    "              >",
-    "            </div>",
-    '            <div class="desc text-muted">',
-    '              <i class="fa-solid fa-truck-medical"></i>',
-    '              <span class="attr">Emergency Service?</span>',
-    '              <span class="val">',
-    '                <i class="fa-solid fa-check text-success"></i>',
-    '                <!-- <i class="fa-solid fa-xmark text-danger"></i> -->',
-    "              </span>",
-    "            </div>",
-    '            <div class="desc text-muted">',
-    '              <i class="fa-solid fa-bed-pulse"></i> 234 Beds',
-    "            </div>",
-    '            <div class="desc text-muted">',
-    '              <span class="price"',
-    '                ><i class="fa-solid fa-tags"></i> Historical Price:',
-    "                $454.79</span",
-    "              >",
-    '              <i class="fa-sharp fa-solid fa-arrow-up text-danger"></i>',
-    "              Compare to recommended price:",
-    "                $254.79</span",
-    "              >",
-    "            </div>",
-    "          </div>",
-    "        </div>",
-  ];
+    if (v.emergency_services == "True") {
+      row.push(`<i class="fa-solid fa-check text-success"></i>`);
+    } else {
+      row.push(`<i class="fa-solid fa-xmark text-danger"></i>`);
+    }
+    row = [
+      ...row,
+      `</span>`,
+      // `</div>`,
+      // `<div class="desc text-muted">`,
+      ` - ${v.beds} <i class="fa-solid fa-bed-pulse"></i>`,
+      `</div>`,
+      `<div class="desc text-muted">`,
+      `<span class="price"`,
+      `><i class="fa-solid fa-tags"></i> Historical Price: ${currencyFormatter.format(
+        v.price
+      )}</span> `,
+      `<i class="fa-sharp fa-solid fa-arrow-up text-danger"></i> `,
+      `Compare to recommended price: ${currencyFormatter.format(
+        v.predicted
+      )}</span>`,
+      `</div>`,
+      `</div>`,
+      `</div>`,
+    ];
+
+    console.log(row);
+    res.append($(row.join("")));
+    counter++;
+    if (counter >= 5) {
+      break;
+    }
+  }
 };
+
+// <div class="result-row row border-bottom p-1 m-1">
+// <div class="hospital p-1">
+//   <div class="name h5">METHODIST HOSPITAL UNION COUNTY</div>
+//   <div class="rating">
+//     <span class="val">
+//       <span class="fa fa-star checked"></span>
+//       <span class="fa fa-star checked"></span>
+//       <span class="fa fa-star checked"></span>
+//       <span class="fa fa-star"></span>
+//       <span class="fa fa-star"></span>
+//     </span>
+//   </div>
+//   <div class="address">
+//     <a
+//       class="text-decoration-none"
+//       target="_blank"
+//       href="https://maps.google.com/?q=METHODIST HOSPITAL UNION COUNTY,4604 US HIGHWAY 60 WEST,MORGANFIELD,Kentucky, KY, 42437"
+//     >
+//       <p>
+//         <i class="fa-solid fa-location-dot text-info"></i>
+//         <span class="text-muted"
+//           >4604 US HIGHWAY 60 WEST, MORGANFIELD, KY 42437 - (123)
+//           456-789
+//         </span>
+//       </p>
+//     </a>
+//   </div>
+
+//   <div class="desc text-muted">
+//     <i class="fa-solid fa-hospital"></i>
+//     <span class="type">Critical Access Hospitals</span>
+//     <span class="ownership"
+//       ><small>- Voluntary non-profit - Church</small></span
+//     >
+//   </div>
+//   <div class="desc text-muted">
+//     <i class="fa-solid fa-truck-medical"></i>
+//     <span class="attr">Emergency Service?</span>
+//     <span class="val">
+//       <i class="fa-solid fa-check text-success"></i>
+//       <!-- <i class="fa-solid fa-xmark text-danger"></i> -->
+//     </span>
+//      -  234 <i class="fa-solid fa-bed-pulse"></i>
+//   </div>
+//   <div class="desc text-muted">
+//   </div>
+//   <div class="desc text-muted">
+//     <span class="price"
+//       ><i class="fa-solid fa-tags"></i> Historical Price:
+//       $454.79</span
+//     >
+//     <i class="fa-sharp fa-solid fa-arrow-up text-danger"></i>
+
+//     Compared to reference price:
+//       $254.79</span
+//     >
+//   </div>
+// </div>
+// </div>
